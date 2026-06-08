c8817e7c00e2ca5ef894484f57a158aa03dd3876
joshidhawal <joshidhawal2046@gmail.com>
2026-06-08 10:08:59 +0530

    Updated Enums for Generic Usage

--- src/enums/enum.ts
+++ src/enums/enum.ts
@@ -1,16 +1,10 @@
-export enum UserEnums {
-  USER_NEW = "N",
-  USER_MODIFIED = "M",
-  USER_DELETED = "D",
-}
-
-export enum AccountEnums {
-  ACCOUNT_NEW = "N",
-  ACCOUNT_MODIFIED = "M",
-  ACCOUNT_DELETED = "D",
-}
-
 export enum UserAuthEnums {
   IS_ADMIN = "Y",
   NOT_ADMIN = "N",
+}
+
+export enum RecordStatus {
+  NEW = "N",
+  MODIFIED = "M",
+  DELETED = "D",
 }
